6804f17acee01b90b69eac80b66101e22539cb04
Adrianna Wiszowata <ada@contractbook.dk>
2025-06-05 20:27:19 +0200

    Initial commit

---
+++ lib/task_sports_feed.ex
@@ -0,0 +1,18 @@
+defmodule TaskSportsFeed do
+  @moduledoc """
+  Documentation for `TaskSportsFeed`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TaskSportsFeed.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/task_sports_feed_test.exs
@@ -0,0 +1,8 @@
+defmodule TaskSportsFeedTest do
+  use ExUnit.Case
+  doctest TaskSportsFeed
+
+  test "greets the world" do
+    assert TaskSportsFeed.hello() == :world
+  end
+end
